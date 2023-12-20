371d0df1accd01568fc6c689f95b0cae8723e5ab
Valtteri Lappalainen <valtteri.lappalainen@gmail.com>
2023-12-20 23:39:31 +0200

    fixed api url

--- client/Sanasoppa.UI/src/app/app.module.ts
+++ client/Sanasoppa.UI/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { API_BASE_URL } from './services/sanasoppa-api.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -11,10 +12,16 @@ import { LobbyComponent } from './views/lobby/lobby.component';
 import { GameComponent } from './views/game/game.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, JoinGameComponent, LobbyComponent, GameComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    JoinGameComponent,
+    LobbyComponent,
+    GameComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
   providers: [
-    { provide: 'API_BASE_URL', useValue: `${window.location.origin}/api` },
+    { provide: API_BASE_URL, useValue: `${window.location.origin}/api` },
   ],
   bootstrap: [AppComponent],
 })
